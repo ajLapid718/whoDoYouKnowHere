673111f4f0db970c40a1a445dc859b91a246bf6b
Allan James Lapid <ajLapid718@gmail.com>
2018-06-12 00:35:12 -0400

    Greet authorized user with the fetched data

--- app.js
+++ app.js
@@ -62,6 +62,18 @@ app.all('/redirect', (req, res) => {
   }
 });
 
+app.get('/user', (req, res) => {
+  request.get({
+    url: 'https://api.github.com/user/public_emails',
+    headers: {
+      Authorization: 'token ' + req.session.access_token,
+      'User-Agent': 'whoDoYouKnowHere-App'
+    }
+  }, (error, response, body) => {
+    res.send("<p>You're logged in! Here's all your emails on GitHub: </p>" + body + '<p>Go back to <a href="/">Log In Page</a></p>')
+  })
+});
+
 app.listen(port, () => {
   console.log('Server is up and running at port ' + port);
 })
